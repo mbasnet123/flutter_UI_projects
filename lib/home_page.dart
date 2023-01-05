5bd18919b276cb39e8d4aacd6027786d223861ea
mbasnet123 <mbasnet868@gmail.com>
2023-01-05 23:07:37 +0545

    added profile app

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,49 +1,162 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-    int _counter = 0;
-
-    void _incrementCounter(){
-      setState(() {
-        print('the value of the counter is $_counter');
-        _counter--;
-        print('the value of the counter after increment is $_counter');
-      });
-  }
-
+class HomePage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        leading: const Icon(Icons.menu),
         title: const Text("My App"),
         centerTitle: true,
+        elevation: 0,
       ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.vertical(
+                    bottom: Radius.circular(16),
               ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
+                color: Theme.of(context).primaryColor,
               ),
-            ],
-          ),
+              padding: EdgeInsets.all(17 ),
+              child: Column(
+                children: <Widget>[
+                  const CircleAvatar(
+                    backgroundImage: AssetImage('assets/images/manish1.png'),
+                    radius: 50,
+                    backgroundColor: Colors.white,
+                  ),
+
+                  const Text(
+                    'Manish Basnet',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Text(
+                    'Flutter Developer',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                          '20',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                      ),
+                      Text(
+                          '43234',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              color: Theme.of(context).primaryColor,
+              padding: EdgeInsets.all(17),
+              width: 110,
+              child: const Text(
+                  "Contact me",
+                  style: TextStyle(color: Colors.white),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: const [
+                CircleAvatar(
+                  backgroundColor: Colors.black,
+                  child: Icon(
+                    Icons.mail_outlined,
+                    color: Colors.white,
+                  ),
+                ),
+              SizedBox(
+                width: 15,
+              ),
+                Text(
+                    'mbasnet868@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                ),
+              ],
+            ),
+
+            const SizedBox(
+              height: 10,
+            ),
+
+            Container(
+              color: Theme.of(context).primaryColor,
+              padding: EdgeInsets.all(17),
+              width: 110,
+              child: const Text(
+                "Contact me",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: const [
+                CircleAvatar(
+                  backgroundColor: Colors.black,
+                  child: Icon(
+                    Icons.call_outlined,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Text(
+                  '983465738',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+
+        const SizedBox(
+          height: 10,
+        ),
+
+        Row(
+          children: const [
+            Text('Follow me'),
+
+          ],
+        )
+
+
+          ],
         ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: (){},
         tooltip: 'Increment',
         child: const Icon(Icons.remove),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter demo',
-      home: HomePage(title: 'MyApp'),
+      theme: ThemeData(primaryColor: Colors.amberAccent),
+      home: HomePage( ),
     );
   }
 }
