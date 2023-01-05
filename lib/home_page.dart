f615c018a8e0e2c5a325e8d53e2caf3c4d62c1ea
mbasnet123 <mbasnet868@gmail.com>
2023-01-05 14:32:08 +0545

    Added increment app

---
+++ lib/home_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+    int _counter = 0;
+
+    void _incrementCounter(){
+      setState(() {
+        print('the value of the counter is $_counter');
+        _counter--;
+        print('the value of the counter after increment is $_counter');
+      });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My App"),
+        centerTitle: true,
+      ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ],
+          ),
+        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.remove),
+      ),
+    );
+  }
+}
